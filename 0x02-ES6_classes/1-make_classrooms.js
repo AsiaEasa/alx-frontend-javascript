0f0503cfedc22f51c98de1b26207b3759ecec8a3
AsiaEasa <asiaeasa89@gmail.com>
2024-06-27 12:23:46 -0700

    done

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,6 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  const R1 = new ClassRoom(19);
-  const R2 = new ClassRoom(20);
-  const R3 = new ClassRoom(34);
-
-  return [R1, R2, R3];
+  const RoomS = [19, 20, 34];
+  return RoomS.map(size => new ClassRoom(size));
 }
