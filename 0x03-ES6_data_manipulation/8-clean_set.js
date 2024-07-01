76c9ef86021e7fd5903a8cbeb9f74b5df5ce8ab3
AsiaEasa <asiaeasa89@gmail.com>
2024-07-01 13:15:50 -0700

    done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,14 @@
 export default function cleanSet(set, startString) {
   if (!startString) return '';
 
-  const filteredValues = Array.from(set)
-    .filter(value => value.startsWith(startString))
-    .map(value => value.slice(startString.length));
+  let R = '';
 
-  return filteredValues.join('-');
+  for (const V of set) {
+    if (V.startsWith(startString)) {
+      if (R) R += '-';
+      R += V.slice(startString.length);
+    }
+  }
+
+  return R;
 }
