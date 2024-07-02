141ddab6272363fa5f8dbf11f9c0af283bff4dd5
AsiaEasa <asiaeasa89@gmail.com>
2024-07-01 22:48:06 -0700

    done

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const Groceries = new Map();
+  Groceries.set('Apples', 10);
+  Groceries.set('Tomatoes', 10);
+  Groceries.set('Pasta', 1);
+  Groceries.set('Rice', 1);
+  Groceries.set('Banana', 5);
+  return Groceries;
+}
